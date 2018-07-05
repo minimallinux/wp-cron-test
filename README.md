58f9d9f7d4d8ba237f86ac641b6ff07f66fb41ac
minimallinux <anthonymcgowan1@gmail.com>
2018-07-05 08:10:33 +0100

    First Commit

---
+++ README.md
@@ -0,0 +1,6 @@
+##WP Cron Test
+
+##Simple output of current wp cron tasks, browse to siteurl/wp-cron.php
+
+
+##Install and activate, then browse as above to check current tasks.
---
+++ wp-cron-test.php
@@ -0,0 +1,8 @@
+<?php
+/*
+Plugin Name: WP-Cron Test
+*/
+echo '<pre>'; print_r( _get_cron_array() ); echo '</pre>';
+function bl_print_tasks() {
+    echo '<pre>'; print_r( _get_cron_array() ); echo '</pre>';
+}
